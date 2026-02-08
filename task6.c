356062d4e6aacf2742768ccf40f26b3cc104d88f
sartirmizi29-sudo <sartirmizi.29@gmail.com>
2026-02-08 19:02:14 +0500

    task6.c

--- task6.c
+++ task6.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Name:\tAbeer\n");
-    printf("Roll No:\t25K-6526\n");
-    printf("Section:\t2A\n");
+    printf("ROLL NUMBER \t NAME \t SECTION\n");
+    printf("25K-6526 \t Abeer \t 2A");
     return 0;
 }
