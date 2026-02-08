800949db59633a1311e388dd51ccd85fb12e81ff
sartirmizi29-sudo <sartirmizi.29@gmail.com>
2026-02-08 19:24:09 +0500

    Add task9.c lab3

---
+++ task9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+float var1;
+
+float var2;
+
+float var3;
+
+printf("Enter a float 1: ");
+
+scanf("%f",&var1);
+
+printf("Enter a float 2: ");
+
+scanf("%f",&var2);
+
+printf("Enter a float 3: ");
+
+scanf("%f",&var3);
+
+printf("%f \n", var1);
+
+printf("%.21f \n", var2);
+
+printf("%.51f \n", var3);
+
+}
